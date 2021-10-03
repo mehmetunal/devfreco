2956471c2b8a57fe41a633c3e35dffdbebf77314
Mehmet <unal.m1991@gmail.com>
2021-10-03 22:11:49 +0300

    Aspect add

---
+++ src/Dev.Cache.Redis/ServiceCollectionExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
+namespace Dev.Cache.Redis
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddPSMoneDistributedRedisCache(
+            this IServiceCollection services, 
+            Action<RedisCacheOptions> options, 
+            params Type[] assemblyPointerTypes)
+        {
+            var redisOptions = new RedisCacheOptions();
+            options.Invoke(redisOptions);
+
+            var multiplexer = ConnectionMultiplexer.Connect(redisOptions.Configuration);
+            services.AddStackExchangeRedisCache(options);
+            services.AddSingleton<IConnectionMultiplexer>(multiplexer);
+            services.AddSingleton<IPSMoneDistributedCache, RedisDistributedCache>();
+            services.DecorateAllInterfacesUsingAspect(assemblyPointerTypes);
+            
+            return services;
+        }
+    }
+}
