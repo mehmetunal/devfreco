31345713d1c6e7b13d1f7f7308cac6e1c1b73fe1
Mehmet <unal.m1991@gmail.com>
2021-10-28 15:13:53 +0300

    bugfix

---
+++ src/Presentation/Dev.Framework/Helper/Docker/DockerHelpers.cs
@@ -0,0 +1,28 @@
+﻿using Dev.Framework.Helper.Configuration;
+using Dev.Framework.Model;
+using Microsoft.Extensions.Configuration;
+
+namespace Dev.Framework.Helper.Docker
+{
+    public class DockerHelpers
+    {
+        public static void UpdateCaCertificates()
+        {
+            "update-ca-certificates".Bash();
+        }
+
+        /// <summary>
+        /// {DockerConfiguration : {UpdateCaCertificate:true}}
+        /// </summary>
+        /// <param name="configuration"></param>
+        public static void ApplyDockerConfiguration(IConfiguration configuration)
+        {
+            var dockerConfiguration = configuration.GetSection(nameof(DockerConfiguration)).Get<DockerConfiguration>();
+
+            if (dockerConfiguration != null && dockerConfiguration.UpdateCaCertificate)
+            {
+                UpdateCaCertificates();
+            }
+        }
+    }
+}
