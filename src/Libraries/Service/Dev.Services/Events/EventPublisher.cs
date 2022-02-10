9660cf6fd48f0b61c5c0ab76388705d7e913522b
Mehmet ÜNAL <mehmet.unal@babil.com>
2022-02-10 11:38:13 +0300

    EventPublisher eklendi.

---
+++ src/Libraries/Service/Dev.Services/Events/EventPublisher.cs
@@ -0,0 +1,51 @@
+﻿using Dev.Core.Infrastructure;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+
+namespace Dev.Services.Events
+{
+    /// <summary>
+    /// Represents the event publisher implementation
+    /// </summary>
+    public partial class EventPublisher : IEventPublisher
+    {
+        #region Methods
+
+        /// <summary>
+        /// Publish event to consumers
+        /// </summary>
+        /// <typeparam name="TEvent">Type of event</typeparam>
+        /// <param name="event">Event object</param>
+        public virtual void Publish<TEvent>(TEvent @event)
+        {
+            //get all event consumers
+            var consumers = EngineContext.Current.ResolveAll<IConsumer<TEvent>>().ToList();
+
+            foreach (var consumer in consumers)
+            {
+                try
+                {
+                    //try to handle published event
+                    consumer.HandleEvent(@event);
+                }
+                catch (Exception exception)
+                {
+                    //log error, we put in to nested try-catch to prevent possible cyclic (if some error occurs)
+                    try
+                    {
+                        EngineContext.Current.Resolve<ILogger>()?.LogError(exception.Message, exception);
+                    }
+                    catch
+                    {
+                        // ignored
+                    }
+                }
+            }
+        }
+
+        
+
+        #endregion
+    }
+}
---
+++ src/Libraries/Service/Dev.Services/Events/EventPublisherExtensions.cs
@@ -0,0 +1,49 @@
+﻿using Dev.Data;
+using Dev.Data.Events;
+using System.Collections.Generic;
+
+namespace Dev.Services.Events
+{
+    /// <summary>
+    /// Event publisher extensions
+    /// </summary>
+    public static class EventPublisherExtensions
+    {
+        /// <summary>
+        /// Entity inserted
+        /// </summary>
+        /// <typeparam name="T">Entity type</typeparam>
+        /// <param name="eventPublisher">Event publisher</param>
+        /// <param name="entity">Entity</param>
+        public static void EntityInserted<T>(this IEventPublisher eventPublisher, T entity) where T : BaseEntity
+        {
+            eventPublisher.Publish(new EntityInsertedEvent<T>(entity));
+        }
+
+        /// <summary>
+        /// Entity updated
+        /// </summary>
+        /// <typeparam name="T">Entity type</typeparam>
+        /// <param name="eventPublisher">Event publisher</param>
+        /// <param name="entity">Entity</param>
+        public static void EntityUpdated<T>(this IEventPublisher eventPublisher, T entity) where T : BaseEntity
+        {
+            eventPublisher.Publish(new EntityUpdatedEvent<T>(entity));
+        }
+        public static void EntityUpdated<T>(this IEventPublisher eventPublisher, List<T> entities) where T : BaseEntity
+        {
+            eventPublisher.Publish(new EntityUpdatedEvent<T>(entities));
+        }
+
+        /// <summary>
+        /// Entity deleted
+        /// </summary>
+        /// <typeparam name="T">Entity type</typeparam>
+        /// <param name="eventPublisher">Event publisher</param>
+        /// <param name="entity">Entity</param>
+        public static void EntityDeleted<T>(this IEventPublisher eventPublisher, T entity) where T : BaseEntity
+        {
+            eventPublisher.Publish(new EntityDeletedEvent<T>(entity));
+        }
+    }
+}
