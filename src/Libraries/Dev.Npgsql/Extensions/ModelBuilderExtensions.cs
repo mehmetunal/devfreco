8acf306152799172776b1450c21546b81bb4fbbe
Mehmet <unal.m1991@gmail.com>
2022-02-13 16:54:16 +0300

    bugfix

--- src/Libraries/Dev.Npgsql/Extensions/ModelBuilderExtensions.cs
+++ src/Libraries/Dev.Npgsql/Extensions/ModelBuilderExtensions.cs
@@ -72,5 +72,15 @@ namespace Dev.Npgsql.Extensions
             var value = enable ? "ON" : "OFF";
             await context.Database.ExecuteSqlRawAsync($"SET IDENTITY_INSERT {entityType.GetSchema()}.{entityType.GetTableName()} {value}");
         }
+
+        public static void EnableIdentityInsert<T>(this DbContext context) => SetIdentityInsert<T>(context, true);
+        public static void DisableIdentityInsert<T>(this DbContext context) => SetIdentityInsert<T>(context, false);
+        private static void SetIdentityInsert<T>([NotNull] DbContext context, bool enable)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+            var entityType = context.Model.FindEntityType(typeof(T));
+            var value = enable ? "ON" : "OFF";
+            context.Database.ExecuteSqlRaw($"SET IDENTITY_INSERT {entityType.GetSchema()}.{entityType.GetTableName()} {value}");
+        }
     }
 }
